cd9b73bfa478bfeae6b467d254ec64d296ab78c1
Beatriz Milz <42153618+beatrizmilz@users.noreply.github.com>
2025-11-14 13:18:05 -0300

    Update 05-importacao.R

--- scripts-pos-aula/05-importacao.R
+++ scripts-pos-aula/05-importacao.R
@@ -9,29 +9,38 @@
 # Importar texto de sites: técnicas de web scraping, como
 # rvest::html_text() (mais avançado)
 
+# Carregar pacotes ------------------
 library(tidyverse)
 library(tidytext)
 library(readtext)
 
-# arquivo PDF de exemplo
+# Ler o arquivo PDF de exemplo ------------------
 arquivo_pdf <- readtext::readtext(
   "referencias/1859-Texto do artigo-3971-2-10-20210608.pdf"
 )
 
+# Preparar tabela ------------------
 df_texto_estruturado <- arquivo_pdf |>
+# adicionar o número da linha como uma coluna nova, chamada n_pagina
   rowid_to_column("n_pagina") |>
+# quebrar o texto sempre que houver uma quebra de linha ("\n" representa quebra de linha)
   mutate(text = str_split(text, "\n")) |>
+# "desaninhar" tabela, teremos uma tabela mais longa
   unnest(text) |>
+# agrupar pelo número da página
   group_by(n_pagina) |>
+# adicionar uma coluna de número da linha
   mutate(n_linha = row_number()) |>
-  ungroup()
-
-df_texto_estruturado |> View()
-
-
-df_texto_limpo <- df_texto_estruturado |>
+# desagrupar
+  ungroup() |>
+# tirar os espaços extras no começo e final
   mutate(text = str_trim(text)) |>
+# remover linhas que estão sem texto nenhum na coluna `text`
   filter(text != "")
+
+
+# Iniciar as técnicas de pré-processamento ------------------
+# A partir deste ponto, usamos as técnicas vistas nas aulas anteriores
 
 contagem_tokens <- df_texto_limpo |>
   unnest_tokens(input = text, output = "word") |>
